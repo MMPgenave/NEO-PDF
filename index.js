d8971c5561060b0fe0534cd5dfa1d8ec7897f9d1
MMPgenave <mohammadmp14@gmail.com>
2023-09-23 10:02:31 +0330

    edit ruler green bar

--- index.js
+++
@@ -1,8 +0,0 @@
-const scales_bar = document.querySelectorAll(".scaled_ruler div");
-const green_bar = document.getElementById("green_bar");
-for (let i = 0; i < scales_bar.length; i++) {
-  if (scales_bar[i].dataset.active === "t") {
-    console.log(scales_bar[i].offsetLeft);
-    green_bar.style.left = scales_bar[i].offsetLeft - 2 + "px";
-  }
-}
